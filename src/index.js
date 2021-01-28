a4f354aaa69350f0d1c2a6a19dcdec90bd4bc037
Noah Roberts <noahmroberts@gmail.com>
2021-01-28 08:00:15 -0600

    added indexhtml

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-cimport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import STORE from './store.js';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
