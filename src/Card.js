6da430c3b470fd43d22efabf3825ab1f805672ab
Noah Roberts <noahmroberts@gmail.com>
2021-01-29 10:55:16 -0600

    Card/List updated

--- src/Card.js
+++ src/Card.js
@@ -4,11 +4,10 @@ import './Card.css';
 function Card(props) {
   return (
     <div className="Card">
-            <button type="button">delete</button>
-            <h3>{props.title}</h3>
-            <p>{props.content}</p>
-          </div>
-
+      <button type="button">delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
   )
 }
 
